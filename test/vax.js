9c75c811291c29c32023084cd19fb4491fdd9940
Chris Contolini <contolini@users.noreply.github.com>
2016-04-08 12:50:40 -0400

    Update tests

--- test/vax.js
+++ test/vax.js
@@ -23,23 +23,4 @@ describe('vax bin', function () {
     });
   });
 
-  it('should return error on missing command', function (done) {
-    this.timeout(4000);
-
-    exec(cmd, function (error, stdout, stderr) {
-      assert(error);
-      assert.equal(error.code, 1);
-      done();
-    });
-  });
-
-  it('should return error on unknown command', function (done) {
-    this.timeout(4000);
-
-    exec(cmd + 'junkcmd', function (error, stdout, stderr) {
-      assert(error);
-      assert.equal(error.code, 1);
-      done();
-    });
-  });
 });
